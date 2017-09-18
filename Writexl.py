95bba302560ff1bddec8d96edcb0b5b1e55ffff1
pino-kim <sungwon.pino@gmail.com>
2017-09-19 01:27:32 +0900

    [add] function add
    set_cell_format
    set_row_size
    set_col_size

--- Writexl.py
+++ Writexl.py
@@ -14,33 +14,52 @@ class Writexl :
         return worksheet
 
     def set_cell_format(self, workbook, type = 'defauilt'):
-        if type == 'test' :
-            format = workbook.add_format({'bold': True, 'font_color': 'red'})
-            return format
-        elif type == 'defauilt' :
-            format = workbook.add_format({'bold': False, 'font_color': 'blue'})
+        if type == 'title':
+            format = workbook.add_format({'bold': True,
+                                          'bg_color':'silver',
+                                          'font_color': 'red',
+                                          'font_size': 15,
+                                          'align': 'center', })
             return format
 
+        elif type == 'label' :
+            format = workbook.add_format({'bold': False,
+                                          #'bg_color':'yellow',
+                                          'font_color': 'blue',
+                                          'font_size': 12, 'align':'center', })
+            return format
+
+        elif type == 'defauilt' :
+            format = workbook.add_format({'bold': False,
+                                          'font_color': 'black'})
+            return format
+
+    def set_row_size(self, worksheet, row_pos, row_size) :
+        worksheet.set_row(row_pos, row_size)
+
+    def set_col_size(self, worksheet, col_start, col_end, col_size, ):
+        worksheet.set_column( col_start + ':' + col_end, col_size)
 
     #def write_cell_w_merge
+
+
 
     def write_cell_by_cellname(self, worksheet, cell_name, content, format):
         worksheet.write(cell_name, content, format)
 
-    def write_cell_by_rowcal(self, worksheet, row, cal, content) :
-        worksheet.write(row, cal, content)
+    def write_cell_by_rowcal(self, worksheet, row, cal, content, format) :
+        worksheet.write(row, cal, content, format)
 
-    def write_data_to_row(self, sheet, start_point, data):
-        sheet.write_row(start_point, data)
+    def write_data_to_row(self, sheet, start_point, data, format):
+        sheet.write_row(start_point, data, format)
 
-    def write_data_to_col(self, sheet, start_point, data):
-        sheet.write_column(start_point, data)
+    def write_data_to_col(self, sheet, start_point, data, format):
+        sheet.write_column(start_point, data, format)
 
 
     def write_xlsx_close(self, workbook):
         workbook.close()
-    #def close_xlsx(self, workbook) :
-    #    workbook.close()
+
 
 
 
--- demo.py
+++ demo.py
@@ -28,18 +28,24 @@ wt_worksheet = writexl.add_xlsx_worksheet(wt_workbook)
 #print(wt_workbook)
 
 # write string 'data length' to A1. Cell point  is (0,0).
-format = writexl.set_cell_format(wt_workbook)
+format = writexl.set_cell_format(wt_workbook, 'title')
+format2 = writexl.set_cell_format(wt_workbook, 'label')
+
+#set row and column size
+writexl.set_row_size(wt_worksheet, 1, 10,)
+writexl.set_col_size(wt_worksheet,'A', 'F', 20)
+
 writexl.write_cell_by_cellname(wt_worksheet ,'A1', 'data length', format)
 
 # write integer 'data length' to A2.  Cell point  is (0,1).
-#writexl.write_cell_by_rowcal(wt_worksheet, 1,0,  data_len)
+writexl.write_cell_by_rowcal(wt_worksheet, 1,0,  data_len, format)
 
 # write string 'data list' to B1. Cell point  is (1,0).
-#writexl.write_cell_by_cellname(wt_worksheet ,'B1', 'data list')
+writexl.write_cell_by_cellname(wt_worksheet ,'B1', 'data list', format)
 
 # write integer data to AB.  Cell point  is (0,1).
 #Write numbers down from cell' B2.'
-#writexl.write_data_to_col(wt_worksheet, 'B2', data)
+writexl.write_data_to_col(wt_worksheet, 'B2', data, format2)
 
 #close workbook
 writexl.write_xlsx_close(wt_workbook)
